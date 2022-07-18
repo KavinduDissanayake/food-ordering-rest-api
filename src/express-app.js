97e3d04a39a75b52aa7cdbe01b8a834d52f8d1c5
Kavindu Dissanayake <51842810+KavinduDissanayake@users.noreply.github.com>
2022-07-19 00:41:13 +0530

    basic express app config done agin

---
+++ src/express-app.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const cors  = require('cors');
+
+
+module.exports = async (app) => {
+
+    app.use(express.json({ limit: '1mb'}));
+    app.use(express.urlencoded({ extended: true, limit: '1mb'}));
+    app.use(cors());
+    app.use(express.static(__dirname + '/public'))
+
+
+}
